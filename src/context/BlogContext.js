a9d0208e6a8639d735f49d9d25feee3ccee89ae3
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-16 20:32:37 +0000

    12.160 Finishing up chapter 12 :)

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -19,27 +19,34 @@ const blogReducer = (state, action) => {
      
         default:
             return state;
-    }
-}
+    };
+};
 
 const addBlogPost = (dispatch) => {
     return (title,content, callback) => {
         dispatch({type: 'add_blogpost', payload:{title, content}})
-        callback();
-    }
-}
+        if (callback){
+            callback()
+        };
+    };
+};
 
 const deleteBlogPost = (dispatch) => {
     return (id) => {
         dispatch({type: 'delete_blogpost', payload: id})
-    }
-}
+    };
+};
 const editBlogPost = (dispatch) => {
-    return (id, title) => {
-        dispatch({type: 'edit_blogpost', payload: {id, title}})
-    }
-}
+    return (id, title, content, callback) => {
+        dispatch({type: 'edit_blogpost',
+                 payload: {id, title, content}});
+                 if (callback){
+                    callback()
+                };
+    };
+};
 
 
 export const {Context, Provider} = 
-    createDataContext(blogReducer, {addBlogPost, deleteBlogPost, editBlogPost}, [{title: 'TEST POST',content: 'TEST CONTENT',id: 1}]);
+    createDataContext(blogReducer, {addBlogPost, deleteBlogPost, editBlogPost},
+         [{title: 'TEST POST',content: 'TEST CONTENT',id: 1}]);
