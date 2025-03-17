ef9b4022288e4895310effdb28337b50d037de85
BogdanFAndrei <bogdan.fandrei@yahoo.com>
2025-03-17 22:38:51 +0000

    Added documentation for showscreen

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -1,11 +1,24 @@
+/**
+ * ShowScreen Component
+ * 
+ * Displays the details of a single blog post including its title and content.
+ * Provides navigation to the Edit screen through a header button.
+ * 
+ * Features:
+ * - Displays blog post title and content
+ * - Edit button in header to modify the post
+ * - Uses BlogContext to access post data
+ */
+
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet,TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {Context} from '../context/BlogContext'
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 
 const ShowScreen = ({navigation}) => {
     const { state } = useContext(Context);
 
+    // Find the specific blog post using the ID from navigation params
     const blogPost = state.find((blogPost) => blogPost.id === navigation.getParam('id'));
 
     return (
@@ -15,6 +28,8 @@ const ShowScreen = ({navigation}) => {
         </View>
     )
 }
+
+// Configure the header with an edit button
 ShowScreen.navigationOptions =({ navigation }) =>{
     return {
         headerRight: () => (
@@ -24,6 +39,7 @@ ShowScreen.navigationOptions =({ navigation }) =>{
         ),
       };
     };
+
 const styles = StyleSheet.create({
   icon: {
     size:35
